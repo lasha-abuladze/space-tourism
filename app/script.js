cdb70f881faf51d27c7c5c5ac6325034c8efcee4
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-04-29 00:04:19 +0400

    fix: resolve layout overflow and page height issues

--- app/script.js
+++ app/script.js
@@ -4,8 +4,24 @@ const btnsNavigationContainer = document.querySelector(`.close-open-btns`);
 const btnsNavigation = document.querySelectorAll(`.btn--menu`);
 const navigation = document.querySelector(`.navigation`);
 
+const mainection = document.querySelector(`.main-section`);
+
+
+
+
+mainection.style.height = `${document.documentElement.scrollHeight}px`;
+
+window.addEventListener(`resize`, () => {
+    mainection.style.height = `${document.documentElement.scrollHeight}px`
+})
+
+
+
 
 btnsNavigationContainer.addEventListener(`click`, (e) => {
     btnsNavigation.forEach(el => el.classList.toggle(`display-none`));
     navigation.classList.toggle(`navigation--closed`);
 })
+
+
+// console.log(document.documentElement.scrollHeight)
