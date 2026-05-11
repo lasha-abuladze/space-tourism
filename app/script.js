2cdd546f3beb9374c4e693bc55758a1682a8bf3b
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-05-11 12:57:07 +0400

    feat: implement logic for switching between pages

--- app/script.js
+++ app/script.js
@@ -3,9 +3,9 @@
 const btnsNavigationContainer = document.querySelector(`.close-open-btns`);
 const btnsNavigation = document.querySelectorAll(`.btn--menu`);
 const navigation = document.querySelector(`.navigation`);
-const sections = document.querySelectorAll(`.section`)
+const sections = document.querySelectorAll(`.section`);
+const navLi = document.querySelectorAll(`.nav-li`);
 
-// const mainection = document.querySelector(`.main-section`);
 
 sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
 
@@ -13,13 +13,44 @@ window.addEventListener(`resize`, () => {
     sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
 })
 
-
-
-
 btnsNavigationContainer.addEventListener(`click`, (e) => {
     btnsNavigation.forEach(el => el.classList.toggle(`display-none`));
     navigation.classList.toggle(`navigation--closed`);
 })
 
 
-// console.log(document.documentElement.scrollHeight)
+
+
+navigation.addEventListener(`click`, (e) => {
+
+    // sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
+    // window.addEventListener(`resize`, () => {
+    //     sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
+    // })
+
+    if(!e.target.closest(`.nav-a`)) return;
+
+    if(e.target.closest(`.nav-a`)) {
+        // sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
+        // window.addEventListener(`resize`, () => {
+        //     sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
+        // })
+
+
+        const sectionName = e.target.closest(`.nav-a`).dataset.sectionTitle;
+
+        sections.forEach(el => {
+            if(!el.classList.contains(`display-none`)) el.classList.add(`display-none`);
+            if(el.dataset.sectionTitle === sectionName) el.classList.remove(`display-none`);
+        })
+
+        sections.forEach(el => {
+            if(el.dataset.sectionTitle === sectionName) el.style.height = `${document.documentElement.scrollHeight}px`;
+        })
+
+        navLi.forEach(el => {
+            if(el.classList.contains(`li-active`)) el.classList.remove(`li-active`);
+        })
+        e.target.closest(`.nav-li`).classList.add(`li-active`);
+    }
+})
