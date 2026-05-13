02600b3eb16bd0a58fde39476e7a5bab47e55d96
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2026-05-13 20:52:27 +0400

    .....

--- app/script.js
+++ app/script.js
@@ -2,9 +2,24 @@
 
 const btnsNavigationContainer = document.querySelector(`.close-open-btns`);
 const btnsNavigation = document.querySelectorAll(`.btn--menu`);
+const btnExplore = document.querySelector(`.btn--explore`);
+
+
 const navigation = document.querySelector(`.navigation`);
 const sections = document.querySelectorAll(`.section`);
 const navLi = document.querySelectorAll(`.nav-li`);
+
+
+let sectionName;
+// let data
+
+// const getData = function() {
+//     fetch(`./app/data.json`).then(res => res.json()).then( d => {
+//         data = d;
+//     });
+// }
+
+getData();
 
 
 sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
@@ -18,26 +33,33 @@ btnsNavigationContainer.addEventListener(`click`, (e) => {
     navigation.classList.toggle(`navigation--closed`);
 })
 
+btnExplore.addEventListener(`click`, () => {
+    sectionName = `destination`;
+
+    sections.forEach(el => {
+        if(!el.classList.contains(`display-none`)) el.classList.add(`display-none`);
+        if(el.dataset.sectionTitle === sectionName) el.classList.remove(`display-none`);
+    })
+
+    navLi.forEach(el => {
+        if(el.classList.contains(`li-active`)) el.classList.remove(`li-active`);
+        if(el.dataset.sectionTitle === sectionName) el.classList.add(`li-active`);
+    })
+
+    // if(!data) alert(`something went wrong ...  try again`);
+    // if(data) console.log(data);
+})
+
 
 
 
 navigation.addEventListener(`click`, (e) => {
 
-    // sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
-    // window.addEventListener(`resize`, () => {
-    //     sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
-    // })
-
     if(!e.target.closest(`.nav-a`)) return;
 
     if(e.target.closest(`.nav-a`)) {
-        // sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
-        // window.addEventListener(`resize`, () => {
-        //     sections.forEach(el => el.style.height = `${document.documentElement.scrollHeight}px`);
-        // })
 
-
-        const sectionName = e.target.closest(`.nav-a`).dataset.sectionTitle;
+        sectionName = e.target.closest(`.nav-a`).dataset.sectionTitle;
 
         sections.forEach(el => {
             if(!el.classList.contains(`display-none`)) el.classList.add(`display-none`);
@@ -54,3 +76,5 @@ navigation.addEventListener(`click`, (e) => {
         e.target.closest(`.nav-li`).classList.add(`li-active`);
     }
 })
+
+
